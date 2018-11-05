a9a4b92d680877f2af637f126461ad7b304d080e
muhammad1339 <muhammadahmed1339@gmail.com>
2018-11-05 20:57:16 +0200

    Adding More Sections

--- app/src/main/java/com/prodev/cvbuilder/data/Constant.java
+++ app/src/main/java/com/prodev/cvbuilder/data/Constant.java
@@ -3,5 +3,7 @@ package com.prodev.cvbuilder.data;
 public class Constant {
     public static final String PREF_FILE = "pref_file";
     public static final String EDUCATION_KEY= "education";
+    public static final String PERSONAL_INFO_KEY= "personal_info";
+    public static final String SKILLS_KEY= "skills";
 }
 
